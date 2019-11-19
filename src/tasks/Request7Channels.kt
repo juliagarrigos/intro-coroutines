0cf0b466337d71c0399e376f12bc178617cbf107
julia-garrigos <julia.garrigos@xing.com>
2019-11-19 17:08:06 +0100

    Task 7 (Channels)

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -1,6 +1,8 @@
 package tasks
 
 import contributors.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
@@ -11,6 +13,27 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+        val repos = service.getOrgReposCallSuspended(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: listOf()
+
+        val channel = Channel<List<User>>()
+
+        repos.map { repo ->
+            launch {
+                log("starting loading for ${repo.name}")
+                val repoUsers = service.getRepoContributorsCallSuspended(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+                channel.send(repoUsers)
+            }
+        }
+
+        var users = emptyList<User>()
+        repeat(repos.size) { iteration ->
+            val repoUsers = channel.receive()
+            users = (users + repoUsers).aggregate()
+            updateResults(users, iteration == repos.lastIndex)
+        }
     }
 }
